ea715143cb7e2579b86a1766a992d7543359580b
Andre Albuquerque <andrealbuquerqueleo@gmail.com>
2025-01-21 01:26:22 -0300

    chore: execute ansible playbook with terraform

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
-# projeto-devops
+# K8S Single Cluster Spinup
 
-![alt text](./images/diagrama.png)
+Personal project to setup a Kubernetes cluster within 3 instances on AWS
+
+## How does it work?
+- Terraform that creates 3 AWS instances, Security Group and a Keypair
+- The Tf.state backend file is stored on an AWS S3 bucket
+- Ansible installs and configures Kubernetes with all dependencies
+- The workflow is automated using Github Actions
+- The Github Actions uses a Docker container with Terraform and Ansible
+
+## Workflow Diagram
+<img src='./images/diagrama.png' alt="Alt Text" width="500" height="350">
--- ec2.tf
+++ ec2.tf
@@ -21,3 +21,36 @@ resource "aws_instance" "kubernetes-master" {
   }
   security_groups = [aws_security_group.k8s-sg.name]
 }
+
+output "ansible_inventory" {
+  value = {
+    all = {
+      hosts = [
+        aws_instance.kubernetes-master[0].public_ip,
+        for worker in aws_instance.kubernetes-worker : worker.public_ip
+      ]
+    }
+    master = {
+      hosts = [
+        aws_instance.kubernetes-master[0].public_ip
+      ]
+    }
+    workers = {
+      hosts = [
+        for worker in aws_instance.kubernetes-worker : worker.public_ip
+      ]
+    }
+  }
+  description = "Dynamic inventory for ansible"
+  sensitive   = false
+}
+
+resource "null_resource" "ansible_playbook" {
+  provisioner "local-exec" {
+    command = "ansible-playbook -i inventory.json -u ubuntu --private-key ~/.ssh/id_rsa --ssh-common-args='-o StrictHostKeyChecking=no' ./ansible/site.yml"
+  }
+  depends_on = [
+    aws_instance.kubernetes-master,
+    aws_instance.kubernetes-worker
+  ]
+}
